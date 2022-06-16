ada79037777e64b409399d803e8b88f867fc4346
Eric Lowe <ericlowe25@gmail.ocm>
2022-06-15 23:43:29 -0400

    Data & Search Context complete

--- src/App.js
+++ src/App.js
@@ -1,51 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
-import {useEffect, useState} from 'react'
-import Gallery from './components/Gallery'
+import React, {useState, useEffect, useRef} from 'react';
+import Gallery from './components/Gallery';
 import SearchBar from './components/SearchBar'
-import {DataContext} from './context/DataContext'
+import { DataContext } from './context/DataContext';
+import { SearchContext } from './context/SearchContext';
 
-const App = () => {
-   let [search, setSearch] = useState('')
-   let [message, setMessage] = useState('Search for Music')
-   let [data, setData] = useState([])
-     
-   const API_URL ='https://itunes.apple.com/search?term='
+function App() {
+  let [message, setMessage] = useState('Search for Music!');
+  let [data, setData] = useState([]);
+  let searchInput = useRef('');
 
-   useEffect(() => {
+  const API_URL = 'https://itunes.apple.com/search?term='
+
+  //handling submit search
+  const handleSearch = (e, term) => {
+    e.preventDefault()
     const fetchData = async () => {
-        document.title = `${search} Music`
-        const response = await fetch(API_URL + search)
-        const resData = await response.json()
-        if (resData.results.length > 0){
-            setData(resData.results)
-        } else {
-            setMessage('Not Found')
-        }
+      document.title = `${term} Music`
+      const response =  await fetch(API_URL + term)
+      const resData = await response.json()
+      // console.log(resData)
+      if (resData.results.length > 0) {
+        setData(resData.results)
+      }
+      else{
+        setMessage('Not Found')
+      }
     }
     fetchData()
-}, [search])
+  }
 
-// Handle submit search
-const handleSearch = (e, term) => {
-  e.preventDefault()
-  setSearch(term)
-}
-
-return (
-  <div className='App'>
-      <SearchBar handleSearch = {handleSearch} />
+  return (
+    <div className="App">
+      <SearchContext.Provider value={{
+        term: searchInput,
+        handleSearch: handleSearch,
+      }}>
+         <SearchBar/>
+      </SearchContext.Provider>
       {message}
       <DataContext.Provider value={data}>
-     
-        <Gallery/>
-     
-     </DataContext.Provider>   
-  </div>
-)
-
+          <Gallery/>
+      </DataContext.Provider>
+      
+    </div>
+  );
 }
 
-export default App;
+export default App
 
 
--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,17 +1,18 @@
-import { useContext } from 'react'
-import { DataContext } from '../context/DataContext'
-import GalleryItem from './GalleryItem'
+import GalleryItem from './GalleryItem';
+import { useContext } from 'react';
+import { DataContext } from '../context/DataContext';
 
-const Gallery= () =>{
-    const data = useContext(DataContext)
-
-    const display = data.map((item, index)=> {
-        return(<GalleryItem item={item} key={index}/>)
-    })  
-
-    return (
+const Gallery = () => {
+    const data = useContext(DataContext);
+    const display = data.map((item, index) => {
+        return (
+            <GalleryItem item={item} key={index}/>
+        )
+    })
+    
+    return(
         <div>
-         {display}
+            {display}
         </div>
     )
 }
