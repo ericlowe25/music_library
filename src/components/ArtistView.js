311e1eaa879898f299e028f93e1ed36189f4f3a9
Eric Lowe <ericlowe25@gmail.ocm>
2022-06-20 20:10:50 -0400

    added new branch

--- src/components/ArtistView.js
+++ src/components/ArtistView.js
@@ -4,13 +4,42 @@ import { useParams } from "react-router-dom"
 function ArtistView(){
     const {id} = useParams()
     const [artistData, setArtistData] = useState([])
+    
+    useEffect(() => {
+        const API_URL = 'http://localhost:4000/album/${id}'
+        const fetchData = async () => {
+            const response = await fetch(API_URL)
+            const resData = await response.json()
+            setArtistData(resData.results)
+        }
+        fetchData()
+    }, [id])
+}
+const allAlbums = artistData.filter(entity => entity.collectionType === 'Album')
+                        .map((album, i) => { 
+                            return (
+                                <div key={i}>
+                                    <Link to={`/album/${album.collectionId}`}>
+                                        <p>{album.collectionName}</p>
+                                    </Link>
+                                </div>)
+                                })
+    
+    const navButtons = () => {
+        return (
+            <div>
+                <button onClick={() => Navigate('/')}>Home</button> | <button onClick={() => Navigate(-1)}>Back</button>
+            </div>
+        )
+    }
 
-    return(
+    return (
         <div>
-            <h2>The id passed was: {id}</h2>
-            <p>Artist Data Goes Here!</p>
+            {artistData.length > 0 ? <h2>{artistData[0].artistName}</h2> : <p>loading...</p>}
+            {navButtons()}
+            {allAlbums}
         </div>
     )
-}
+
 
 export default ArtistView
